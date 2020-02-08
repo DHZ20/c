55b15aef6ed0feb56f688759f542d5b04ec0c3e5
lin chuan <linch1982@gmail.com>
2020-02-09 01:02:48 +0800

    add ycm config

--- projects/a.c
+++ projects/a.c
@@ -1,7 +1,17 @@
-/*  */#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    struct Node* pre;
+    struct Node* next;
+    int data;
+};
 
 int main(int argc, char** argv)
 {
+    struct Node head;
+    head.next = (struct Node*)malloc(sizeof(struct Node));
+    head.next->next->next = NULL;
     printf("Hello, world!\n");
     return 0;
 }
