0ae9a75e6be20b3a55f58c6c8ede7da26c2bcabc
lin chuan <linch1982@gmail.com>
2020-03-27 00:21:19 +0800

    add tang

---
+++ c-projects/han/var.c
@@ -0,0 +1,25 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  var.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/26/2020 01:02:47 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  lin chuan , linch1982@gmail.com
+ *   Organization:  
+ *
+ * ============================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char** argv) {
+    {
+        int a = 10;
+    }
+    printf("a is %d\n", a);
+}
